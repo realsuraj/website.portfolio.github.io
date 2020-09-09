257577ce7b247d1c34efcdad6222e752564a01dc
realsuraj <66870141+realsuraj@users.noreply.github.com>
2020-09-09 13:14:42 +0530

    update 7

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-$('#navbar a, .btn').on('click',function(event){
-    if(this.hash !==''){
-        event.preventDefault();
-        const hash = this.hash;
-        $('html,body').animate(
-        {
-            scrollTop: $(hash).offset().top - 80
-        },
-        800
-        );
-    }
+$('#navbar a, .btn').on('click', function(event) {
+	if (this.hash !== '') {
+		event.preventDefault();
+		const hash = this.hash;
+		$('html,body').animate(
+			{
+				scrollTop: $(hash).offset().top - 80
+			},
+			800
+		);
+	}
+});
+
+$(document).ready(function() {
+	AOS.init();
 });
